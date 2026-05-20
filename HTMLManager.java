210d4fc13d1919bacfb74d009da9fb93365bd53e
Milady-velasquez <ken.velasquezmorales@shoreline.edu>
2026-05-20 11:32:43 -0700

    HTMLManager (Queue<HTMLTag>html)

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
+   private Queue<HTMLTag> tags; //internal field to remember the tags
+  
+   public HTMLManager(Queue<HTMLTag> html){
+      
+      if (html == null){ //null checker
+         throw new IllegalArgumentException("Invalid Queue(Can't be null)"); 
+      }
+      
+      //copy of the values into the new Queue
+      this.tags = new LinkedList<>(html);
+  }
+  
+  
   
 }
