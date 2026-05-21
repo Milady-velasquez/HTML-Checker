b4cd0a7d592a19e1d022b29b7c030683214cd0a8
Milady-velasquez <ken.velasquezmorales@shoreline.edu>
2026-05-21 09:55:38 -0700

    FixHTML method

--- HTMLManager.java
+++ HTMLManager.java
@@ -33,7 +33,58 @@ public class HTMLManager {
       }
       return str;
    }
-          
+   
+   public void fixHTML() {
+    Queue<HTMLTag> fixedQueue = new LinkedList<>();
+    
+    // stack to track open tags
+    Stack<HTMLTag> openTagsStack = new Stack<>();
+    
+    //Process every tag in the current manager queue
+    int initialSize = this.tags.size();
+    for (int i = 0; i < initialSize; i++) {
+        HTMLTag current = this.tags.remove();
+        
+        if (current.isSelfClosing()) {  
+        //Self-closing tags go straight to the right queue
+            fixedQueue.add(current);
+        } 
+        else if (current.isOpening()) {
+            // Opening tags go to both the queue and the stack
+            fixedQueue.add(current);
+            openTagsStack.push(current);
+        } 
+        else if (current.isClosing()) {
+            if (!openTagsStack.isEmpty()) {
+                HTMLTag topOpen = openTagsStack.peek();
+                
+                if (topOpen.matches(current)) {
+                    // if it is perfect match, Keep the closing tag and pop the open tag
+                    fixedQueue.add(current);
+                    openTagsStack.pop();
+                } else {
+                    // if it is Mismatch, Fix it by automatically closing the top open tag
+                    HTMLTag correctCloseTag = topOpen.getMatching();
+                    fixedQueue.add(correctCloseTag);
+                    openTagsStack.pop();
+                    
+                    // Put the current closing tag back in rotation by backing up the loop
+                    this.tags.add(current);
+                    initialSize++; 
+                }
+            }
+        }
+    }
+    
+    //Clean up any leftover open tags that were never closed
+    while (!openTagsStack.isEmpty()) {
+        HTMLTag leftOverOpen = openTagsStack.pop();
+        fixedQueue.add(leftOverOpen.getMatching());
+    }
+    
+    //Update the manager's master queue with the fixed version
+    this.tags = fixedQueue;
+}          
 }
 
 
